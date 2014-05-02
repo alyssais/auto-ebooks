edb71d13e54bb3f1886b0498a2a827be7dff3ea0
Ross Penman <ross@pen.mn>
2014-05-02 23:34:52 +0100

    Added a controller for accounts

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,20 @@
+class AccountsController < ApplicationController
+  def new
+  end
+
+  def create
+    auth = env["omniauth.auth"]
+    @account = Account.find_or_initialize_by(uid: auth["uid"])
+    @account.username = auth["info"]["nickname"]
+    @account.name = auth["info"]["name"]
+    @account.token = auth["credentials"]["token"]
+    @account.secret = auth["credentials"]["secret"]
+    @account.save!
+  end
+
+  def update
+    @account = Account.find(params[:id])
+    @account.update based_on: @account.client.user(params[:account][:based_on])[:id]
+    @account.update_model # TODO: delayed_job
+  end
+end
