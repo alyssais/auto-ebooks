eeba1e4b00c471b4b6c2c7c8542219d1d9c9a67e
Ross Penman <ross@pen.mn>
2014-05-02 18:49:27 +0100

    Added Twitter integration

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,12 @@
 class Account < ActiveRecord::Base
   validates_presence_of :uid, :username, :token, :secret
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.secrets.consumer_key
+      config.consumer_secret = Rails.application.secrets.consumer_secret
+      config.access_token = token
+      config.access_token_secret = secret
+    end
+  end
 end
