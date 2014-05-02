6fc070d5271d48f37056dd99679a58bf63141870
Alyssa Ross <hi@alyssa.is>
2014-05-02 18:49:27 +0100

    Added Twitter integration

---
+++ spec/factories.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :account do
+    sequence(:uid)
+    name "Test Account"
+    sequence(:username) { |n| "test_account#{n}" }
+    token { SecureRandom.hex }
+    secret { SecureRandom.hex }
+
+    factory :valid_account do
+      %w[uid username token secret].each do |field|
+        send(field, Rails.application.secrets.test_account.send(:[], field))
+      end
+    end
+  end
+end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -5,4 +5,11 @@ describe Account do
   it { should validate_presence_of :username }
   it { should validate_presence_of :token }
   it { should validate_presence_of :secret }
+
+  it "can post a tweet" do
+    account = FactoryGirl.create(:valid_account)
+    content = SecureRandom.uuid
+    account.client.update(content)
+    account.client.user_timeline.first[:text].should == content
+  end
 end
