993a09a417fff5986b36ddee380b6f5b8ded472b
Ross Penman <ross@pen.mn>
2014-05-02 22:30:41 +0100

    Installed OmniAuth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, Rails.application.secrets.consumer_key, Rails.application.secrets.consumer_secret
+end
