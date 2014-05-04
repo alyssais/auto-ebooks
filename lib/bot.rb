264ee6caa7e4f7a2ee1b12cadf2ccb648e96ab01
Alyssa Ross <hi@alyssa.is>
2014-05-04 01:34:31 +0100

    Fixed some bot bugs

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,4 +1,6 @@
 class Bot < Ebooks::Bot
+  attr :running
+
   def initialize(account)
     @account = account
 
@@ -15,28 +17,30 @@ class Bot < Ebooks::Bot
       end
 
       bot.on_follow do |user|
-        bot.follow(user[:screen_name])
+				unless user[:id_str] == account.uid
+					bot.follow(user[:screen_name])
+				end
       end
 
       bot.on_mention do |tweet, meta|
         if should_reply? tweet
-          reply(tweet, meta)
+          auto_reply(tweet, meta)
         end
       end
 
       bot.on_timeline do |tweet, meta|
         if should_reply? tweet
-          multiplier = tweet[:user][:id_str] == based_on
+          multiplier = tweet[:user][:id_str] == account.based_on ? 2 : 1
           tokens = Ebooks::NLP.tokenize(tweet[:text])
           tokens.map! { |t| t.downcase.gsub(/[^\w]/, "") }
 
           reply_threshold = 10 * multiplier
           fav_threshold = 100 * multiplier
 
-          keywords = model.keywords.top(fav_threshold).map { |t| t.to_s.downcase }
+          keywords = account.model.keywords.top(fav_threshold).map { |t| t.to_s.downcase }
 
           if (keywords.first(reply_threshold) & tokens).count > 0
-            reply(tweet, meta)
+            auto_reply(tweet, meta)
           elsif (keywords & tokens).count > 0
             bot.twitter.favorite(tweet[:id])
           end
@@ -47,23 +51,29 @@ class Bot < Ebooks::Bot
         bot.tweet(account.model.make_statement)
       end
 
-      def reply(tweet, meta)
-        reply_to = meta[:reply_prefix].gsub(/@/, "").split(/\s/)
-        reply_to.select! do |username|
-          @client.friendship(username, account.username)[:relationship][:source][:followed_by]
-        end
-        reply_prefix = reply_to.map { |u| "@#{u}" }.join(" ") + " "
-        length = 140 - reply_prefix
-        response = account.model.make_response(tweet[:text], length)
-        bot.reply(tweet, response)
-      end
+   end
+  end
 
-      def should_reply?(tweet_or_dm)
-        user = tweet_or_dm[:sender] || tweet_or_dm[:user]
-        should_reply = true
-        should_reply &= !user[:screen_name].end_with?("_ebooks")
-        should_reply &= !tweet_or_dm[:retweeted]
-      end
+  def auto_reply(tweet, meta)
+    reply_to = meta[:reply_prefix].gsub(/@/, "").split(/\s/)
+    reply_to.select! do |username|
+      tweet[:user][:screen_name] == username || @account.client.friendship(username, @account.username)[:source][:followed_by]
     end
+    reply_prefix = reply_to.map { |u| "@#{u}" }.join(" ") + " "
+    length = 140 - reply_prefix.length
+    response = @account.model.make_response(tweet[:text], length)
+    reply(tweet, reply_prefix + response)
+  end
+
+  def should_reply?(tweet_or_dm)
+    user = tweet_or_dm[:sender] || tweet_or_dm[:user]
+    should_reply = true
+    should_reply &= !user[:screen_name].end_with?("_ebooks")
+    should_reply &= !tweet_or_dm[:retweeted]
+  end
+
+  def start
+    @running = true
+    super
   end
 end
